e88251266b1de96f62a4109200ce6a34fa715c31
colson1111 <colson1111@gmail.com>
2015-08-27 16:58:18 -0400

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,33 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. the animal at 1
+print(animals[1])
+# python
+
+# 2. the third animal
+print(animals[2])
+# peacock
+
+# 3. the first animal
+print(animals[0])
+# bear
+
+# 4. the animal at 3
+print(animals[3])
+# kangaroo
+
+# 5. the fifth animal
+print(animals[4])
+# whale
+
+# 6. the animal at 2
+print(animals[2])
+# peacock
+
+# 7. the sixth animal
+print(animals[5])
+# platypus
+
+# 8. the animal at 4
+print(animals[4])
+# whale
